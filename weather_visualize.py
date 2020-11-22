289be6cb472b370df9fd91f56f6415ef6aa6f977
Bex Tuychiev <bex@ibexprogramming.com>
2020-11-22 08:03:12 +0500

    feature: Create a function to load data inside run_app

--- weather_visualize.py
+++ weather_visualize.py
@@ -12,6 +12,25 @@ def main():
 
 
 def run_app():
+    """
+    A function to run
+    the main part of the program
+    """
+
+    @st.cache
+    def load_data(path):
+        """
+        A function load data
+        :param path: a path to the file source
+        :return: pandas.DataFrame instance
+        """
+        df = pd.read_csv(path)
+        return df
+
+
+
+    # Load cities data with locations
+    cities = load_data('data/worldcities.csv')
 
     # Set a title
     st.title('Visualize Weather Patterns')
@@ -21,10 +40,10 @@ def run_app():
                       ['Custom Country Input', 'Choose From Dropdown'])
 
     # Depending on action
-    # if action == 'Custom Country Input':
-    #     country_input = st.text_input('Enter country (String matching '
-    #                                   'is enabled under the hood):', max_chars=60)
-    #     with st.spinner('Matching the closest country name'):
+    if action == 'Custom Country Input':
+        country_input = st.text_input('Enter country (String matching '
+                                      'is enabled under the hood):', max_chars=60)
+        with st.spinner('Matching the closest country name'):
 
 
 if __name__ == '__main__':
